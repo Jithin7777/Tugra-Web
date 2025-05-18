b29c20387ed821b6c6795abbac1c498d9917b41a
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-05-18 16:01:09 +0530

    Make archives section mobile-responsive with 2 image columns

--- src/app/(section)/ArchivesSection.tsx
+++ src/app/(section)/ArchivesSection.tsx
@@ -17,7 +17,7 @@ const ArchivesSection = () => {
           {Array.from({ length: 12 }).map((_, i) => (
             <span
               key={i}
-              className="block -mt-12 tracking-[-20px] text-[135px] bg-gradient-to-b from-[#9B834E] to-[#352D1B] bg-clip-text text-transparent"
+              className="block -mt-7  md:-mt-12 tracking-[-10px]    md:tracking-[-20px] text-[80px]   md:text-[135px] bg-gradient-to-b from-[#9B834E] to-[#352D1B] bg-clip-text text-transparent"
             >
               TUGRA
             </span>
@@ -25,55 +25,57 @@ const ArchivesSection = () => {
         </div>
 
         {/* Column 2: 3 Images */}
-        <div className="flex flex-col gap-6 items-center">
-          <Image
-            src="/images/archives/img1.png"
-            alt="Archive 1"
-            width={400}
-            height={250}
-            className=" object-cover w-[400px] h-[400px]"
-          />
-          <Image
-            src="/images/archives/img3.png"
-            alt="Archive 2"
-            width={400}
-            height={250}
-            className=" object-cover w-[400px] h-[400px]"
-          />
-          <Image
-            src="/images/archives/img2.png"
-            alt="Archive 3"
-            width={400}
-            height={130}  // half height of 250
-            className=" object-cover w-[400px] h-[200px]"
-          />
-        </div>
-
-        {/* Column 3: 3 Images */}
-        <div className="flex flex-col gap-6 items-center">
-          <Image
-            src="/images/archives/img4.png"
-            alt="Archive 4"
-            width={400}
-            height={250}
-            className=" object-cover w-[400px] h-[400px]"
-          />
-          <Image
-            src="/images/archives/img5.png"
-            alt="Archive 5"
-            width={400}
-            height={250}
-            className=" object-cover w-[400px] h-[400px]"
-          />
-          <Image
-            src="/images/archives/img6.png"
-            alt="Archive 6"
-            width={400}
-            height={130}  // half height of 250
-            className=" object-cover w-[400px] h-[200px]"
-          />
-        </div>
-      </div>
+<div className="grid grid-cols-2 gap-4 sm:gap-6 col-span-2">
+            <div className="flex flex-col gap-6 items-center">
+              <Image
+                src="/images/archives/img1.png"
+                alt="Archive 1"
+                width={400}
+                height={250}
+                className=" object-cover w-[200px] h-[200px] md:w-[400px] md:h-[400px] "
+              />
+              <Image
+                src="/images/archives/img3.png"
+                alt="Archive 2"
+                width={400}
+                height={250}
+                className=" object-cover w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+              />
+              <Image
+                src="/images/archives/img2.png"
+                alt="Archive 3"
+                width={400}
+                height={130}  // half height of 250
+                className=" object-cover  w-[200px] h-[100px]   md:w-[400px] md:h-[200px]"
+              />
+            </div>
+    
+            {/* Column 3: 3 Images */}
+            <div className="flex flex-col gap-6 items-center">
+              <Image
+                src="/images/archives/img4.png"
+                alt="Archive 4"
+                width={400}
+                height={250}
+                className=" object-cover w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+              />
+              <Image
+                src="/images/archives/img5.png"
+                alt="Archive 5"
+                width={400}
+                height={250}
+                className=" object-cover w-[200px] h-[200px] md:w-[400px] md:h-[400px]"
+              />
+              <Image
+                src="/images/archives/img6.png"
+                alt="Archive 6"
+                width={400}
+                height={130}  // half height of 250
+                className="  w-[200px] h-[100px]   md:w-[400px] md:h-[200px]"
+              />
+            </div>
+    
+</div>      </div>
     </section>
   );
 };
