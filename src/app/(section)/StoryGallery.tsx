b5fa9fdb3df60cea9fd6739db064b3e4d2b47f06
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-05-19 16:43:14 +0530

    update with next/img

--- src/app/(section)/StoryGallery.tsx
+++ src/app/(section)/StoryGallery.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Instagram } from "lucide-react";
+import Image from "next/image";
 
 const StoryGallery = () => {
   return (
     <section className="bg-black text-white py-16 px-4 md:px-5">
-     <h2 className="text-2xl uppercase md:text-4xl tracking-[5px] md:tracking-[10.59px] font-light text-right mb-16 font-open-sans text-[#C9AB81] md:-translate-x-20 -translate-x-0">
-  scroll our stories
-</h2>
-
+      <h2 className="text-2xl uppercase md:text-4xl tracking-[5px] md:tracking-[10.59px] font-light text-right mb-16 font-open-sans text-[#C9AB81] md:-translate-x-20 -translate-x-0">
+        scroll our stories
+      </h2>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
-        <img
+        <Image
           src="/images/story-gallery/img1.png"
           alt="Image 1"
           className="w-full h-68 object-cover"
+          width={400}
+          height={200}
         />
 
         <div className="flex flex-col items-center justify-center  text-white h-64 w-full rounded-md">
@@ -23,33 +25,43 @@ const StoryGallery = () => {
           </p>
         </div>
 
-        <img
+        <Image
           src="/images/story-gallery/img2.png"
           alt="Image 3"
           className="w-full h-68 object-cover"
+          width={400}
+          height={200}
         />
-        <img
+        <Image
           src="/images/story-gallery/img3.png"
           alt="Image 4"
           className="w-full h-68 object-cover  "
+          width={400}
+          height={200}
         />
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-        <img
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <Image
           src="/images/story-gallery/img4.png"
           alt="Image 5"
           className="col-span-1 w-full h-68 object-cover"
+          width={400}
+          height={200}
         />
-        <img
+        <Image
           src="/images/story-gallery/img5.png"
           alt="Image 6"
           className="col-span-2 w-full h-68 object-cover"
+          width={400}
+          height={200}
         />
-        <img
+        <Image
           src="/images/story-gallery/img6.png"
           alt="Image 7"
           className="col-span-1 w-full h-68 object-cover"
+          width={400}
+          height={100}
         />
       </div>
     </section>
